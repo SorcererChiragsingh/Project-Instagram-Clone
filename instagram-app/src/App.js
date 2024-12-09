6d046f0681799a0f59fc680950b4d34283b90eb7
Chirag singh <chiragsingh8926@gmail.com>
2024-12-09 20:59:31 +0530

    Firebase connection problem

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,6 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import { Suspense, lazy } from "react";
+
+const Login = lazy(() => import("./pages/login") );
+
 function App() {
   
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Suspense fallback = {<p>Loading .... </p>}>
+      <Routes>
+        <Route path= {"/login"} elements={<Login />} />
+      </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
---
+++ instagram-app/src/pages/login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const login = () => {
+  return <div>This is a login page</div>;
+  
+};
+
+export default login;
